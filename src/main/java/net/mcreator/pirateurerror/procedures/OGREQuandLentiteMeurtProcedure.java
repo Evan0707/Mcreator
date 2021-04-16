0e4595eae8abe1442d070dc16df90d3e1a7403e0
Evan0707 <evan.gery@gmx.fr>
2021-04-16 22:50:45 +0200

    dernière vers du mod pour se soire + ajout dant pas implémenté en jeu + ogre fonctionelle

--- src/main/java/net/mcreator/pirateurerror/procedures/OGREQuandLentiteMeurtProcedure.java
+++ src/main/java/net/mcreator/pirateurerror/procedures/OGREQuandLentiteMeurtProcedure.java
@@ -1,44 +1,26 @@
 package net.mcreator.pirateurerror.procedures;
 
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.Entity;
+
+import net.mcreator.pirateurerror.PirateurErrorModElements;
+
+import java.util.Map;
+
 @PirateurErrorModElements.ModElement.Tag
 public class OGREQuandLentiteMeurtProcedure extends PirateurErrorModElements.ModElement {
-
 	public OGREQuandLentiteMeurtProcedure(PirateurErrorModElements instance) {
 		super(instance, 2);
-
 	}
 
 	public static void executeProcedure(Map<String, Object> dependencies) {
-		if (dependencies.get("x") == null) {
-			if (!dependencies.containsKey("x"))
-				System.err.println("Failed to load dependency x for procedure OGREQuandLentiteMeurt!");
+		if (dependencies.get("entity") == null) {
+			if (!dependencies.containsKey("entity"))
+				System.err.println("Failed to load dependency entity for procedure OGREQuandLentiteMeurt!");
 			return;
 		}
-		if (dependencies.get("y") == null) {
-			if (!dependencies.containsKey("y"))
-				System.err.println("Failed to load dependency y for procedure OGREQuandLentiteMeurt!");
-			return;
-		}
-		if (dependencies.get("z") == null) {
-			if (!dependencies.containsKey("z"))
-				System.err.println("Failed to load dependency z for procedure OGREQuandLentiteMeurt!");
-			return;
-		}
-		if (dependencies.get("world") == null) {
-			if (!dependencies.containsKey("world"))
-				System.err.println("Failed to load dependency world for procedure OGREQuandLentiteMeurt!");
-			return;
-		}
-
-		double x = dependencies.get("x") instanceof Integer ? (int) dependencies.get("x") : (double) dependencies.get("x");
-		double y = dependencies.get("y") instanceof Integer ? (int) dependencies.get("y") : (double) dependencies.get("y");
-		double z = dependencies.get("z") instanceof Integer ? (int) dependencies.get("z") : (double) dependencies.get("z");
-		IWorld world = (IWorld) dependencies.get("world");
-
-		if (world instanceof World && !world.getWorld().isRemote) {
-			world.getWorld().createExplosion(null, (int) x, (int) y, (int) z, (float) 3, Explosion.Mode.BREAK);
-		}
-
+		Entity entity = (Entity) dependencies.get("entity");
+		if (entity instanceof PlayerEntity)
+			((PlayerEntity) entity).addExperienceLevel((int) 12);
 	}
-
 }
