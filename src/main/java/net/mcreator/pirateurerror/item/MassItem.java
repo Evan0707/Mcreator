14490d68a593bdc0d263db271383691dd98451dc
Evan0707 <evan.gery@gmx.fr>
2021-04-17 10:07:29 +0200

    dernier vers

---
+++ src/main/java/net/mcreator/pirateurerror/item/MassItem.java
@@ -0,0 +1,51 @@
+
+package net.mcreator.pirateurerror.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.item.SwordItem;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.IItemTier;
+
+import net.mcreator.pirateurerror.PirateurErrorModElements;
+
+@PirateurErrorModElements.ModElement.Tag
+public class MassItem extends PirateurErrorModElements.ModElement {
+	@ObjectHolder("pirateur_error:mass")
+	public static final Item block = null;
+	public MassItem(PirateurErrorModElements instance) {
+		super(instance, 3);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new SwordItem(new IItemTier() {
+			public int getMaxUses() {
+				return 100;
+			}
+
+			public float getEfficiency() {
+				return 4f;
+			}
+
+			public float getAttackDamage() {
+				return 2f;
+			}
+
+			public int getHarvestLevel() {
+				return 1;
+			}
+
+			public int getEnchantability() {
+				return 2;
+			}
+
+			public Ingredient getRepairMaterial() {
+				return Ingredient.EMPTY;
+			}
+		}, 3, -3f, new Item.Properties().group(ItemGroup.TOOLS)) {
+		}.setRegistryName("mass"));
+	}
+}
